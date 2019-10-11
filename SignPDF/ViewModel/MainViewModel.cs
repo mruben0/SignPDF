b47c15919fc9cb3d533608ce271c0417273f63e5
Ruben Martirosyan <mruben93@gmail.com>
2019-10-11 16:09:25 +0400

    Add metro, create viewmodel

--- SignPDF/ViewModel/MainViewModel.cs
+++ SignPDF/ViewModel/MainViewModel.cs
@@ -1,34 +1,94 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
+using SignPDF.Services.Abstract;
+using System.Windows.Input;
 
 namespace SignPDF.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
+   
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
-        public MainViewModel()
+        private readonly IPDFSigningService _signingService;
+        private string _inputPath;
+        private string _outPath;
+        private string _imagePath;
+
+        public MainViewModel(IPDFSigningService signingService)
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            if (IsInDesignMode)
+            {
+                return;
+            }
+
+            _signingService = signingService ?? throw new System.ArgumentNullException(nameof(signingService));
         }
+
+        public ICommand GetPathCommand => new RelayCommand<object>((o) =>
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            switch (o)
+            {
+                case "file":
+                    openFileDialog.Filter = "Pdf Files|*.pdf";
+                    if (openFileDialog.ShowDialog() == true)
+                    {
+                        InputPath = openFileDialog.FileName;
+                    }
+                    break;
+                case "image":
+                    openFileDialog.Filter = "Image Files(*.bmp; *.jpg; *.jpeg,*.png) | *.BMP;*.JPG;*.JPEG;*.PNG";
+                    if (openFileDialog.ShowDialog() == true)
+                    {
+                        ImagePath = openFileDialog.FileName;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        public ICommand SignCommand => new RelayCommand(() =>
+        {
+            SaveFileDialog sfdl = new SaveFileDialog();
+            sfdl.Filter = "Pdf Files|*.pdf";
+            if (sfdl.ShowDialog() == true)
+            {
+                OutPath = sfdl.FileName;
+                _signingService.Sign(InputPath, OutPath, ImagePath, true);
+            }
+        });
+
+        public string InputPath
+        {
+            get { return _inputPath; }
+            set
+            {
+                _inputPath = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string OutPath
+        {
+            get { return _outPath; }
+            set
+            {
+                _outPath = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string ImagePath
+        {
+            get { return _imagePath; }
+            set
+            {
+                _imagePath = value;
+                RaisePropertyChanged();
+            }
+        }
+
     }
 }
